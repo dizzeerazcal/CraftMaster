0c39acc744dda3d4c7b84696f08a4187faad5903
dizzeerazcal <daniel.rost@gmx.de>
2014-04-15 19:02:56 +0200

    First Commit

---
+++ src/com/example/craftmaster/Craft.java
@@ -0,0 +1,32 @@
+package com.example.craftmaster;
+
+public class Craft {
+	
+	private int index;
+	private String text;
+	
+	public Craft(int index, String text){
+		this.index = index;
+		this.text = text;
+		
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+
+}
